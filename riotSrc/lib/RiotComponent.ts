dd3664ea40735899dd63f3157f71f01dc5e44696
David Spires <rr505398@TX0053AM033953.prscoad.com>
2018-12-04 17:31:49 -0600

    going in a diff direction

--- riotSrc/lib/RiotComponent.ts
+++ riotSrc/lib/RiotComponent.ts
@@ -2,27 +2,6 @@ import {Component} from "./Component";
 import * as riot from "riot";
 
 // @ts-ignore
-export class RiotComponent extends Component {
-    constructor(private _tagName: string,
-                private _html: string,
-                private _css: string,
-                private _attrs: string,
-                private _componentClass: any) {
-        //Extend Super Class
-        super(_tagName, _html, _css, _attrs, _componentClass);
-    }
-
-    mount() {
-        //Mount using riot
-        riot.tag(this._tagName, this._html, this._css, this._attrs, this._componentClass);
-    }
-
-    unmount() {
-
-    }
-
-    update() {
-
-    }
-
+export class RiotComponent extends riot.Tag {
+    //More info at the bottom: https://riot.js.org/api/#manual-construction
 }
